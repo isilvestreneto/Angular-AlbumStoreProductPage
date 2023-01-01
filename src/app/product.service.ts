7ee0159a88c3e4d7f51cb42c2e94f92fcc04ab0c
Ivan Silvestre <isilvestreneto@gmail.com>
2023-01-01 16:25:32 -0300

    [2] - correcoes variaveis

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,19 +1,14 @@
-import 'rxjs/add/operator/map';
-import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import "rxjs/add/operator/map";
+import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
 
 @Injectable()
 export class ProductService {
+  private _albumUrl = "../assets/album.json";
 
-  private _albumUrl = '../assets/album.json';
-
-  constructor(private _http: Http) { }
+  constructor(private _http: Http) {}
 
   getAlbum(id: number) {
-
-
-    return this._http.get(this._albumUrl).map(res => res.json());
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
-
-
 }
